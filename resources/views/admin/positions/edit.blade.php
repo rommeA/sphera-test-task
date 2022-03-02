31cac90fdce5384ffc160b5a14e69e793a7ccea8
Anna Romme <62960969+rommeA@users.noreply.github.com>
2022-03-02 15:52:23 +0300

    initial commit

---
+++ resources/views/admin/positions/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.backend')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @include('admin.sidebar')
+
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">Edit Position #{{ $position->id }}</div>
+                    <div class="card-body">
+                        <a href="{{ url('/admin/positions') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                        <br />
+                        <br />
+
+                        @if ($errors->any())
+                            <ul class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
+
+                        {!! Form::model($position, [
+                            'method' => 'PATCH',
+                            'url' => ['/admin/positions', $position->id],
+                            'class' => 'form-horizontal',
+                            'files' => true
+                        ]) !!}
+
+                        @include ('admin.positions.form', ['formMode' => 'edit'])
+
+                        {!! Form::close() !!}
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
